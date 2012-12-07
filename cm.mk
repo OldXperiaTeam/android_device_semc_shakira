472134aa675b88df6ad86c0f9499dd31db0f7efc
Daveee10 <daver16@msn.com>
2012-12-07 18:09:37 +0100

    Initial commit for Android 4.2

---
+++ cm.mk
@@ -0,0 +1,24 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common JBMiniProject and CM stuff.
+# $(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/semc/shakira/shakira.mk)
+
+# Setup device configuration
+PRODUCT_RELEASE_NAME := X8
+PRODUCT_DEVICE := shakira
+PRODUCT_NAME := cm_shakira
+PRODUCT_BRAND := SEMC
+PRODUCT_MODEL := X8
+PRODUCT_MANUFACTURER := Sony Ericsson
+
+# Release data
+TARGET_NO_LIVEWALLPAPERS := false
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+TARGET_INCLUDE_GAPPS := falsee
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E15i
